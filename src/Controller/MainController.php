88009d18f52808cc1e7166168efd1279236f8887
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-24 03:10:18 +0200

    add routes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,18 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController
 {
-    public function home(): Response
+    #[Route('/home', name: 'home')]
+    public function home(Request $request): Response
     {
-        return new Response();
+        return new Response('/home');
     }
 
+    #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
-        return new Response();
+        return new Response('/contact');
     }
 
     public function priceList(): Response
