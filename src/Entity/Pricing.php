64bf6071f8c4ca200ab0fbfaf126a30243a23575
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-05 15:16:16 +0200

    setters and getters added

--- src/Entity/Pricing.php
+++ src/Entity/Pricing.php
@@ -11,4 +11,38 @@ class Pricing
     private null|int $maxPrice = 0;
 
     private null|int $deposit = 0;
+
+    public function getMinPrice(): ?int
+    {
+        return $this->minPrice;
+    }
+
+    public function setMinPrice($minPrice): self
+    {
+        $this->minPrice = $minPrice;
+        return $this;
+    }
+
+    public function getMaxPrice(): ?int
+    {
+        return $this->maxPrice;
+    }
+
+    public function setMaxPrice($maxPrice): self
+    {
+        $this->maxPrice = $maxPrice;
+        return $this;
+    }
+
+    public function getDeposit(): ?int
+    {
+        return $this->deposit;
+    }
+
+    public function setDeposit($deposit): self
+    {
+        $this->deposit = $deposit;
+        return $this;
+    }
+
 }
