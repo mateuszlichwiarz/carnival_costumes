ef163b7131c58da7d79551cdf7d2590c98058823
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-05 02:45:56 +0200

    pricing entity created

---
+++ src/Entity/Pricing.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Pricing
+{
+    private null|int $minPrice = 0;
+
+    private null|int $maxPrice = 0;
+
+    private null|int $deposit = 0;
+}
