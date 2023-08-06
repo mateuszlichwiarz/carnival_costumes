656d5aebfc02ed4e490c1e6265e7efa20a9f3210
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-06 02:49:37 +0200

    events entity created

---
+++ src/Entity/Events.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Events
+{
+    private ?string $type;
+
+    private ?string $description;
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
