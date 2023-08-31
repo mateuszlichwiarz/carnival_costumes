d2300740cbda0dab5a75ac5894ba0f3769d9434f
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-31 22:14:46 +0200

    rename admin controller

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class DashboardController extends AbstractController
 {
     #[Route('/admin', name: 'admin_index')]
     public function index()
