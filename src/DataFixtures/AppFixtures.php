e7c0e869b766ae5cd15c2f561895b5833c978f8b
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-31 04:16:09 +0200

    add hasher to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,31 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+use App\Entity\Admin;
+
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new Admin();
+
+        $password = 'password';
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            $password
+        );
+
+        $admin->setPassword($hashedPassword);
+        $admin->setUsername('mateusz');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
