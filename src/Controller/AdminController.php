dd0d1703932942f07f8a048b2bba52a98442bdb4
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-08-31 04:13:36 +0200

    fix: route

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('admin', name: 'admin_index')]
+    #[Route('/admin', name: 'admin_index')]
     public function index()
     {   
         return $this->render('admin/index.html.twig');
