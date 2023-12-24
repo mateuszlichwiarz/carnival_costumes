c24399ae441b7dd99723807a8dbb75bd6cb43e2b
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-12-24 04:53:21 +0100

    create date entity

---
+++ src/Date/Date.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Date;
+
+use DateTime;
+
+use App\Date\WeeksCalculatorTrait;
+
+class Date
+{
+    protected ?int $day;
+
+    protected ?int $week;
+
+    protected ?int $month;
+
+    protected ?int $year;
+
+    use WeeksCalculatorTrait;
+
+    public function __construct(public DateTime $dateTime)
+    {
+        $this->day   = intval($dateTime->format('d'));
+        $this->month = intval($dateTime->format('m'));
+        $this->week  = $this->calculateWeek($this->getDay());
+        $this->year  = intval($dateTime->format('Y'));
+    }
+
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    public function getWeek(): int
+    {
+        return $this->week;
+    }
+
+    public function getMonth(): int
+    {
+        return $this->month;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+}
