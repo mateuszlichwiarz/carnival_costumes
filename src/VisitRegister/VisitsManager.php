ef47d457c267fd26aae0ee709a344842dd4e91a6
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-12-25 06:29:42 +0100

    change name visit facade

---
+++ src/VisitRegister/VisitsManager.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VisitRegister;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+use App\VisitRegister\SessionRegister;
+use App\VisitRegister\VisitsRecorder;
+
+class VisitsManager
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private SessionRegister $sessionRegister,
+        private VisitsRecorder $visitsRecorder,
+    ){}
+
+    public function saveVisit(): void
+    {
+        if($this->getSessionRegistered() === null) {
+            $this->visitsRecorder->saveVisit();
+            $this->sessionRegister->register();
+        }
+    }
+
+    public function getSessionRegistered()
+    {
+        return $this->requestStack->getSession()->get('registered');
+    }
+
+}
