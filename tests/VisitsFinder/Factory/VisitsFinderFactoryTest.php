0846862973592a6a1968e755ab60aea5c4a25bd1
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-24 23:40:49 +0100

    tests: creating visits finder tests

---
+++ tests/VisitsFinder/Factory/VisitsFinderFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\VisitsFinder\Factory;
+
+use App\Date\CurrentDate\CurrentDateFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+use App\VisitsFinder\Factory\WeekVisitsFinderFactory;
+use App\VisitsFinder\Finders\WeekVisitsFinder;
+use App\Date\Date;
+
+final class VisitsFinderFactoryTest extends TestCase
+{
+    private array $visitsCollection;
+
+    private Date $date;
+
+    public function setUp(): void
+    {
+        $this->visitsCollection = [1 => 'string'];
+
+        $dateFactory = new CurrentDateFactory();
+        $this->date = $dateFactory->createDate();
+    }
+
+    public function testCanCreateVisitsFinder()
+    {
+
+        $visitsFinderFactory = new WeekVisitsFinderFactory();
+        $visitsFinder = $visitsFinderFactory->createFinder($this->date, $this->visitsCollection);
+
+        return $this->assertInstanceOf(WeekVisitsFinder::class, $visitsFinder);
+    }
+}
