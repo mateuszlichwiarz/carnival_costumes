de7ac33534bbb6120e0996924a1b7aad55c7132c
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-25 21:40:31 +0100

    create week visits finder

---
+++ src/VisitsFinder/Finders/WeekVisitsFinder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VisitsFinder\Finders;
+
+use App\VisitsFinder\Finders\Interface\OneResultVisitsFinderInterface as VisitsFinderInterface;
+
+use App\Date\Date;
+
+use App\Entity\Visits;
+
+final class WeekVisitsFinder implements VisitsFinderInterface
+{
+    public function __construct(
+        private Date $date,
+        private array $visitsCollection,
+    ){}
+
+    public function find(): Visits
+    {
+        foreach($this->visitsCollection as $visit) {
+            if($this->date->getYear()  === $visit->getYear() &&
+               $this->date->getMonth() === $visit->getMonth() &&
+               $this->date->getWeek()  === $visit->getWeek()
+             ) {
+                $foundedVisits = $visit;
+            }
+        }
+        return $foundedVisits;
+    }
+}
