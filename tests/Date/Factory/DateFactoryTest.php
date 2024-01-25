6d24d981e14d59c26ec284cb5e7d52a9b5c08d82
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-25 14:51:45 +0100

    tests: date factory tests

---
+++ tests/Date/Factory/DateFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Date\Factory;
+
+use PHPUnit\Framework\TestCase;
+
+use App\Date\CustomDate\CustomDateFactory;
+use App\Date\CurrentDate\CurrentDateFactory;
+use App\Date\Date;
+
+final class DateFactoryTest extends TestCase
+{
+    public function testCanCreateCustomDateFactory(): void
+    {
+        $customDateFactory = new CustomDateFactory();
+        $customDate = $customDateFactory->createDate('20-12-2020');
+
+        $this->assertInstanceOf(Date::class, $customDate);
+    }
+
+    public function testExpectExceptionCustomDateFactory(): void
+    {
+        $customDateFactory = new CustomDateFactory();
+
+        $this->expectException(\Exception::class);
+        $customDateFactory->createDate();
+    }
+
+    public function testCanCreateCurrentDateFactory(): void
+    {
+        $currentDateFactory = new CurrentDateFactory();
+        $currentDate = $currentDateFactory->createDate();
+
+        $this->assertInstanceOf(Date::class, $currentDate);
+    }
+}
