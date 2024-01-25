dc7e914b467bd1591e20c736fe85dbd2434f80e5
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-26 00:43:43 +0100

    create month finder

---
+++ src/VisitsFinder/Finders/MonthVisitsFinder.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VisitsFinder\Finders;
+
+use App\VisitsFinder\Finders\Interface\ManyResultsVisitsFinderInterface as VisitsFinderInterface;
+
+use App\Date\Date;
+
+final class MonthVisitsFinder implements VisitsFinderInterface
+{
+    public function __construct(
+        private Date $date,
+        private array $visitsCollection,
+    ){}
+    
+    public function find(): array
+    {
+        foreach($this->visitsCollection as $visit) {
+            if($this->date->getYear() === $visit->getYear() &&
+               $this->date->getMonth() === $visit->getMonth()
+             ) {
+                $foundedVisits[] = $visit;
+            }
+        }
+
+        return $foundedVisits;
+    }
+}
