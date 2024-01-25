5af14f3fff89b69c2bdb2bbaf6fa173576dd2e26
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-25 21:21:00 +0100

    create interface for VisitsFinder factories

---
+++ src/VisitsFinder/Factory/VisitsFinderFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VisitsFinder\Factory;
+
+use App\VisitsFinder\Finders\Interface\VisitsFinderInterface;
+use App\Date\Date;
+
+interface VisitsFinderFactoryInterface
+{
+    public function createFinder(Date $date, array $visitsCollection): VisitsFinderInterface;
+}
