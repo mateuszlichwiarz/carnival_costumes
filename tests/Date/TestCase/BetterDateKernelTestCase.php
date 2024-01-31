1a3f3462213d6fb425461857f986f2e3a327ac74
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-01-31 03:04:02 +0100

    tests: use interface better date in test case

--- tests/Date/TestCase/BetterDateKernelTestCase.php
+++ tests/Date/TestCase/BetterDateKernelTestCase.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace App\Tests\Date\TestCase;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use App\Date\BetterDate;
+use App\Date\BetterDateInterface;
 
 abstract class BetterDateKernelTestCase extends KernelTestCase
 {
-    protected BetterDate $betterDate;
+    protected BetterDateInterface $betterDate;
 
     public function setUp(): void
     {
         self::bootKernel();
-        $this->betterDate = self::getContainer()->get(BetterDate::class);
+        $this->betterDate = self::getContainer()->get(BetterDateInterface::class);
     }
 }
