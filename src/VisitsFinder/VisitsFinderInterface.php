8d302cd0e2152a2b8041b567292b46b5a9f065d4
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-07 23:14:21 +0100

    create visits finder interface

---
+++ src/VisitsFinder/VisitsFinderInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\VisitsFinder;
+
+use App\Date\Entity\Date;
+use App\Entity\Visits;
+
+interface VisitsFinderInterface
+{
+    public function setup(array $visitsCollection, Date $date = null): void;
+
+    public function findWeek(): Visits;
+}
