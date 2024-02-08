f952b5417cdd9e536d6b2e091d3b7abb087c0005
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-08 23:33:43 +0100

    tests: improve current date test

--- tests/Date/BetterDateTest.php
+++ tests/Date/BetterDateTest.php
@@ -4,56 +4,41 @@ declare(strict_types=1);
 
 namespace App\Tests\Date;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Date\Factory\CurrentDateFactory;
 use App\Tests\Date\TestCase\BetterDateKernelTestCase;
-
-use App\Date\BetterDate;
 use App\Date\Entity\Date;
 
 class BetterDateTest extends BetterDateKernelTestCase
 {
-    private Date $date;
+    private Date $properCurrentDate;
 
     public function setUp(): void
     {
         parent::setUp();
-    }
-
-    public function testCreateCustomDate(): void
-    {
-        $date = $this->betterDate->create('14-02-2006');
-
-        $this->assertInstanceOf(Date::class, $date);
+        $this->properCurrentDate = self::getContainer()->get(CurrentDateFactory::class)->create();
     }
 
     public function testCreateCurrentDate(): void
     {
+        $this->assertInstanceOf(Date::class, $this->betterDate->create());
+    }
+
+    public function testCurrentDate(): void
+    {
         $date = $this->betterDate->create();
-
-        $this->assertInstanceOf(Date::class, $date);
+        $this->assertEquals($date->getWeek(), $this->properCurrentDate->getWeek());
     }
 
-    public function testCustomGetWeek(): void
+    public function testCreateCustomDate(): void
     {
-        $date = $this->betterDate->create('14-02-2006');
-        $week = $date->getWeek();
-
-        $this->assertEquals($week, 2);
+        $this->assertInstanceOf(Date::class, $this->betterDate->create('14-02-2006'));
     }
 
-    public function testCustomGetMonth(): void
+    public function testCustomDateAsserts(): void
     {
         $date = $this->betterDate->create('14-02-2006');
-        $month = $date->getMonth();
-
-        $this->assertEquals($month, 2);
-    }
-
-    public function testCustomGetYear(): void
-    {
-        $date = $this->betterDate->create('14-02-2006');
-        $year = $date->getYear();
-
-        $this->assertEquals($year, 2006);
+        $this->assertEquals($date->getWeek(), 2);
+        $this->assertEquals($date->getMonth(), 2);
+        $this->assertEquals($date->getYear(), 2006);
     }
 }
