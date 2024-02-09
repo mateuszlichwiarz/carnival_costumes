f406af0891a28f7a1ddb92dd0059d2f0d1f47e3d
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-09 01:27:24 +0100

    refactor: separate logic operation on collection in finders

--- src/VisitsFinder/Finders/YearVisitsFinder.php
+++ src/VisitsFinder/Finders/YearVisitsFinder.php
@@ -10,19 +10,31 @@ use App\Date\Entity\Date;
 
 final class YearVisitsFinder implements VisitsFinderInterface
 {
+    private ?array $foundedVisits = null;
+
     public function __construct(
         private Date $date,
         private array $visitsCollection,
-    ){}
+    ){
+        $this->collectionOperation();
+    }
     
     public function find(): array
+    {
+        if(!is_null($this->foundedVisits)) {
+            return $this->foundedVisits;
+        }else {
+            throw new \Exception('no found');
+        }
+    }
+
+    private function collectionOperation(): void
     {
         foreach($this->visitsCollection as $visit) {
             if($this->date->getYear() === $visit->getYear()
              ) {
-                $foundedVisits[] = $visit;
+                $this->foundedVisits[] = $visit;
             }
         }
-        return $foundedVisits;
     }
 }
