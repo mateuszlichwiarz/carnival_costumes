e20de23bfbc631d841ab5f8b8f1d97e6f6f2e9fd
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-16 01:51:47 +0100

    create date form type

---
+++ src/Form/Type/SelectVisitsType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Doctrine\DBAL\Types\DateTimeImmutableType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\WeekType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SelectVisitsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('search', SubmitType::class)
+        ;
+    }
+}
