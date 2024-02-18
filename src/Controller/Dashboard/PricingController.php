0370e898980f0992248187ee4154a80437956054
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-18 04:47:14 +0100

    dashboard pricing controller with update pricing action

---
+++ src/Controller/Dashboard/PricingController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Pricing;
+use App\Form\Type\UpdatePricingType;
+
+class PricingController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {}
+
+    #[Route('/dashboard/pricing', name: 'dashboard_pricing')]
+    public function updatePricingAction(Request $request): Response
+    {
+        $pricing = new Pricing();
+
+        $form = $this->createForm(UpdatePricingType::class, $pricing);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $pricing = $form->getData();
+
+            $this->entityManager->persist($pricing);
+            $this->entityManager->flush();
+            $this->addFlash('success', 'write');
+        }else {
+            $this->addFlash('error', 'no write');
+        }
+
+        return $this->render('dashboard/pricing.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
