a85f9a5045490a418b5a405a99e14d52b8c27349
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-19 04:20:12 +0100

    form: change day choices typo to int

--- src/Form/Type/UpdateOpenHoursType.php
+++ src/Form/Type/UpdateOpenHoursType.php
@@ -20,13 +20,13 @@ class UpdateOpenHoursType extends AbstractType
         $builder
             ->add('day', ChoiceType::class, [
                 'choices' => [
-                    'Monday'    => 'Monday',
-                    'Tuesday'   => 'Tuesday',
-                    'Wednesday' => 'Wednesday',
-                    'Thursday'  => 'Thursday',
-                    'Friday'    => 'Friday',
-                    'Saturday'  => 'Saturday',
-                    'Sunday'    => 'Sunday'
+                    'Monday'    => 1,
+                    'Tuesday'   => 2,
+                    'Wednesday' => 3,
+                    'Thursday'  => 4,
+                    'Friday'    => 5,
+                    'Saturday'  => 6,
+                    'Sunday'    => 7
                 ]
             ])
             ->add('isClosed', CheckboxType::class, [
