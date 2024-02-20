6183f93b8183d7ace5b4e5ec0daafc394fecf919
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-20 01:59:42 +0100

    inject security bundle to dashboard controller

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace App\Controller\Dashboard;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractController
 {
+    private readonly UserInterface $admin;
+
+    public function __construct(private Security $security)
+    {
+        $this->admin = $this->security->getUser();
+    }
+
     #[Route('/dashboard', name: 'dashboard_index')]
     public function indexAction()
     {
-        return $this->render('dashboard/index.html.twig');
+        return $this->render('dashboard/index.html.twig', [
+            'admin' => $this->admin,
+        ]);
     }
 }
