9768523ef51c60269e36797bae45f139e2962fe4
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-20 19:39:51 +0100

    refactor: better name form class

--- src/Form/Type/OpenHoursUpdateType.php
+++ src/Form/Type/OpenHoursUpdateType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
-class UpdateOpenHoursType extends AbstractType
+class OpenHoursUpdateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
