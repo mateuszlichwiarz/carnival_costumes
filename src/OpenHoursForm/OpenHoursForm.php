754ad2aa650ed43803ba7e1c74bd655f1bb53e94
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-23 23:26:10 +0100

    refactor: create fasade for processing request data

---
+++ src/OpenHoursForm/OpenHoursForm.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OpenHoursForm;
+
+use App\Entity\OpenHours;
+use App\Form\Type\OpenHoursUpdateType;
+use App\OpenHoursForm\Persister\OpenHoursPersister;
+use App\OpenHoursForm\DataContainer\OpenHoursDataContainerInterface;
+use App\OpenHoursForm\Resolver\OpenHoursUpdateResolver;
+use App\OpenHoursForm\OpenHoursFormInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class OpenHoursForm implements OpenHoursFormInterface
+{
+    private Form $form;
+
+    private OpenHours $openHours;
+
+    public function __construct(
+        private OpenHoursDataContainerInterface $openHoursDataContainer,
+        private OpenHoursUpdateResolver $openHoursUpdateResolver,
+        private FormFactoryInterface $formFactory,
+        private OpenHoursPersister $openHoursPersister,
+        )
+    {}
+
+    public function createForm(): void
+    {
+        $this->openHours = new OpenHours();
+
+        $this->form = $this->formFactory->create(
+            OpenHoursUpdateType::class,
+            $this->openHours
+        );
+    }
+
+    public function handleRequest(Request $request): OpenHours|null
+    {
+        $this->form->handleRequest($request);
+        if($this->form->isSubmitted() && $this->form->isValid())
+        {
+            $this->openHours = $this->form->getData();
+            return $this->openHoursUpdateResolver->resolve($this->openHours);
+        }else{
+            return null;
+        }
+    }
+
+    public function persist(OpenHours $openHours): void
+    {
+        $this->openHoursPersister->persist($openHours);
+    }
+
+    public function viewForm(): FormInterface
+    {
+        return $this->form;
+    }
+
+    public function daysContainer(): OpenHoursDataContainerInterface
+    {
+        return $this->openHoursDataContainer;
+    }
+}
