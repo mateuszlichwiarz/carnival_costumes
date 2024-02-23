3e46d41b4b0f6a4e69495be82f5bac9ba9fffd4f
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-23 02:41:28 +0100

    refactor: create persister for open hours object

---
+++ src/OpenHoursForm/Persister/OpenHoursPersister.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OpenHours\Persister;
+
+use App\Entity\OpenHours;
+use Doctrine\ORM\EntityManagerInterface;
+
+class OpenHoursPersister
+{
+    public function __construct(private EntityManagerInterface $manager)
+    {}
+
+    public function persist(OpenHours $openHours): void
+    {
+        $this->manager->persist($openHours);
+        $this->manager->flush();
+    }
+
+    public function remove(OpenHours $openHours): void
+    {
+        $this->manager->remove($openHours);
+        $this->manager->flush();
+    }
+}
