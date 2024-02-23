dc3145ead2a90461f623b2462fb48aa160d24306
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-23 23:59:00 +0100

    refactor: remove old form processing and implement OpenHoursForm in controller

--- src/Controller/Dashboard/ContactController.php
+++ src/Controller/Dashboard/ContactController.php
@@ -4,51 +4,32 @@ declare(strict_types=1);
 
 namespace App\Controller\Dashboard;
 
-use App\Repository\OpenHoursRepository;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use App\Entity\OpenHours;
-
-use App\Form\Type\OpenHoursUpdateType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\OpenHoursForm\OpenHoursForm;
 
 class ContactController extends AbstractController
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
-        private OpenHoursRepository $openHoursRepository,
+        private OpenHoursForm $openHoursForm,
     ){}
 
     #[Route('/dashboard/contact', name: 'dashboard_contact_index')]
     public function updateContactAction(Request $request): Response
-    {
-        $allDaysOpenHours = $this->openHoursRepository->findAllDaysOpenHours();
-        $openHours = new OpenHours();
-        $form = $this->createForm(OpenHoursUpdateType::class, $openHours);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $openHours = $form->getData();
-            $foundOpenHoursDay = $this->openHoursRepository->findDayOpenHours($openHours->getDay());
-            if($foundOpenHoursDay !== null) {
-                $foundOpenHoursDay
-                    ->setEndDate($openHours->getEndDate())
-                    ->setStartDate($openHours->getStartDate())
-                    ->setIsClosed($openHours->getIsClosed())
-                ;
-                $this->entityManager->persist($foundOpenHoursDay);
-            }else{
-                $this->entityManager->persist($openHours);
-            }
-            $this->entityManager->flush();
+    { 
+        $this->openHoursForm->createForm();
+        $openHours = $this->openHoursForm->handleRequest($request);
+        if($openHours !== null) {
+            $this->openHoursForm->persist($openHours);
             return $this->redirectToRoute('dashboard_contact_index');
         }
-
+        
         return $this->render('dashboard/contact.html.twig', [
-            'form' => $form,
-            'daysOpenHours' => $allDaysOpenHours,
+            'form' => $this->openHoursForm->viewForm(),
+            'daysOpenHours' => $this->openHoursForm->daysContainer()->getAllDays(),
         ]);
     }
 }
