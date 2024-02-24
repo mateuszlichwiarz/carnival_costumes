f9cf49cafadbcee94e0641b8ffb6c0c2ad3cb2f2
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-24 13:08:47 +0100

    fixtures: fix visits load

--- src/DataFixtures/VisitsFixtures.php
+++ src/DataFixtures/VisitsFixtures.php
@@ -9,25 +9,18 @@ use App\Entity\Visits;
 
 class VisitsFixtures extends Fixture
 {
-
     public function load(ObjectManager $manager): void
     {
-        $date = strtotime('2022-02');
-
-        for($k = 2022; $k <= 2026; $k++)
-        {
-            for($j = 1; $j <= 12; $j++)
-            {
-                for($i = 1; $i <= 5; $i++)
-                {
-                    $visitors = new Visits();
+        for($k = 2022; $k <= 2026; $k++) {
+            for($j = 1; $j <= 12; $j++) {
+                for($i = 1; $i <= 5; $i++) {
+                    $visits = new Visits();
+                    $visits->setMonth($j);
+                    $visits->setWeek($i);
+                    $visits->setYear($k);
+                    $visits->setVisits(rand(0, 100));
         
-                    $visitors->setMonth($j);
-                    $visitors->setWeek($i);
-                    $visitors->setYear($k);
-                    $visitors->setVisits(rand(0, 100));
-        
-                    $manager->persist($visitors);
+                    $manager->persist($visits);
                     $manager->flush();
                 }
             }
