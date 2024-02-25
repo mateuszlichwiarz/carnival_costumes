efdef6dd1c77a5f8fcd3605bb7350a23bd83fcc8
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-25 18:24:18 +0100

    separete view forms

--- src/Form/Contact/ContactUpdateFormInterface.php
+++ src/Form/Contact/ContactUpdateFormInterface.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App\Form\Contact;
 
+use Symfony\Component\Form\FormInterface;
+
 interface ContactUpdateFormInterface
 {
     public function createForms(): void;
 
     public function handleRequest();
 
-    public function viewForms();
+    public function viewAddressForm(): FormInterface;
+
+    public function viewPhoneForm(): FormInterface;
 
     public function getContact();
 }
