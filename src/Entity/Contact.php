4aac96a3cdb5ba2240bc655646e20b03c1613914
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-25 17:47:40 +0100

    fix: id can be null

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -13,7 +13,7 @@ class Contact
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private int $id = null;
+    private ?int $id = null;
 
     #[ORM\Column]
     private string $city;
