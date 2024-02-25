d19aab0c910e6e45defaeeca071e070a530e55ea
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-25 23:16:21 +0100

    fix: handle request must return Contact object or null

--- src/Form/Contact/ContactUpdateFormInterface.php
+++ src/Form/Contact/ContactUpdateFormInterface.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Form\Contact;
 
+use App\Entity\Contact;
+
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 interface ContactUpdateFormInterface
 {
     public function createForms(): void;
 
-    public function handleRequest();
+    public function handleRequest(Request $request): Contact|null;
 
     public function viewAddressForm(): FormInterface;
 
     public function viewPhoneForm(): FormInterface;
-
-    public function getContact();
 }
