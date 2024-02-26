97c47a3202fc509d006bcd09a76b66af777185bc
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-26 04:18:43 +0100

    refactor: inject repositories

--- src/Controller/Page/MainController.php
+++ src/Controller/Page/MainController.php
@@ -4,89 +4,70 @@ declare(strict_types=1);
 
 namespace App\Controller\Page;
 
+use App\VisitRegister\VisitsRegister;
+
+use App\Repository\ContactRepository;
 use App\Repository\OpenHoursRepository;
 use App\Repository\PricingRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Entity\OpenHours;
-use App\Entity\Pricing;
-use App\Repository\ContactRepository;
-use App\VisitRegister\VisitsRegister;
-
-
 class MainController extends AbstractController
 {
     public function __construct(
         private VisitsRegister $visitsRegister,
+        private ContactRepository $contactRepository,
+        private PricingRepository $pricingRepository,
     ){
         $this->visitsRegister->saveVisit();
     }
 
     #[Route('/index', name: 'index')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $session = $request->getSession();
-
         return $this->redirectToRoute('home');
     }
 
     #[Route('/home', name: 'home')]
-    public function home(Request $request): Response
+    public function homeAction(Request $request): Response
     {
-        $message = '/home';
-
-        $contact = ['street' => 'holdu pruskiego'
-            ,'city' => 'klodzko'
-        ];
-
         return $this->render('page/home.html.twig', [
-            'message' => $message,
-            'contact' => $contact,
+            'contact' => $this->contactRepository->findOneContact(),
         ]);
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(
-        OpenHoursRepository $openHoursRepository,
-        ContactRepository $contactRepository,
-    ): Response
+    public function contactAction(OpenHoursRepository $openHoursRepository): Response
     {
         return $this->render('page/contact.html.twig', [
             'daysOpenHours' => $openHoursRepository->findAllDaysOpenHours(),
-            'contact' => $contactRepository->findOneContact(),
+            'contact' => $this->contactRepository->findOneContact(),
         ]);
     }
 
     #[Route('/pricing', name: 'pricing')]
-    public function pricing(
-        PricingRepository $pricingRepository
-        ): Response
+    public function pricingAction(): Response
     {
         return $this->render('page/pricing.html.twig', [
-            'pricing' => $pricingRepository->findOnlyOnePricing(),
+            'pricing' => $this->pricingRepository->findOnlyOnePricing(),
         ]);
     }
 
     #[Route('/gallery', name: 'gallery')]
-    public function gallery(): Response
+    public function galleryAction(): Response
     {
-        $message = '/gallery';
-
-        return $this->render('page/gallery.html.twig', [
-            'message' => $message,
-        ]);
+        return $this->render('page/gallery.html.twig');
     }
 
     #[Route('/statute', name: 'statute')]
-    public function statute(): Response
+    public function statuteAction(): Response
     {
-        $message = '/statute';
-
         return $this->render('page/statute.html.twig', [
-            'message' => $message,
+            'contact' => $this->contactRepository->findOneContact(),
+            'pricing' => $this->pricingRepository->findOnlyOnePricing(),
         ]);
     }
 }
