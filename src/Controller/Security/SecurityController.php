6c0d991c2c904fdfabafe8edc15a75b57282b8ed
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-27 00:05:19 +0100

    inject security user to security controller

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -6,9 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class SecurityController extends AbstractController
 {
+    private ?UserInterface $admin = null;
+
+    public function __construct(private Security $security)
+    {
+        $this->admin = $this->security->getUser();
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -21,7 +30,11 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'admin' => $this->admin,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
