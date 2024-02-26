93898d472e14ceeeb971903b946aeeaf4e4a4e73
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-02-26 03:55:08 +0100

    fix: change phone field to telType

--- src/Form/Contact/Type/ContactPhoneUpdateType.php
+++ src/Form/Contact/Type/ContactPhoneUpdateType.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Form\Contact\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ContactPhoneUpdateType extends AbstractType
@@ -14,7 +14,9 @@ class ContactPhoneUpdateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Phone', NumberType::class)
+            ->add('Phone', TelType::class, [
+                'label' => 'Phone number'
+            ])
             ->add('Set', SubmitType::class)
         ;
     }
