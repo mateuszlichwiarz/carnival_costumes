efe570303718cc4590f98f238c75700c36b6edfd
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-27 02:11:12 +0100

    fixtures: session visits entity

--- src/DataFixtures/VisitsFixtures.php
+++ src/DataFixtures/VisitsFixtures.php
@@ -5,22 +5,16 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-use App\Entity\Visits;
+use Hume\SessionVisitsBundle\Entity\Visits;
 
 class VisitsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for($k = 2022; $k <= 2026; $k++) {
-            for($j = 1; $j <= 12; $j++) {
-                for($i = 1; $i <= 5; $i++) {
-                    $visits = new Visits();
-                    $visits->setMonth($j);
-                    $visits->setWeek($i);
-                    $visits->setYear($k);
-                    $visits->setVisits(rand(0, 100));
-        
-                    $manager->persist($visits);
+        for($year = 2022; $year <= 2026; $year++) {
+            for($month = 1; $month <= 12; $month++) {
+                for($week = 1; $week <= 5; $week++) {
+                    $manager->persist(new Visits($month, $week, $year, rand(0, 100)));
                     $manager->flush();
                 }
             }
