b75c9a6986067c528d191695bc1cfa51fe9cfded
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2024-03-29 04:36:33 +0100

    add credentials to app:create-admin command

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -8,6 +8,7 @@ use App\Entity\Admin;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -26,30 +27,39 @@ class CreateAdminCommand extends Command
     ){
         parent::__construct();
     }
-    
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $admin = new Admin();
-
-        $password = 'admin2';
-
-        $hashedPassword = $this->passwordHasher->hashPassword(
-            $admin,
-            $password
-        );
-        $admin->setPassword($hashedPassword);
-        $admin->setUsername('admin2');
-        $admin->setRoles(['ROLE_ADMIN']);
-
-        $this->manager->persist($admin);
-        $this->manager->flush();
-        
-        return Command::SUCCESS;
-    }
 
     protected function configure(): void
     {
         $this->setDescription('Creates administrator');
         $this->setHelp('This command create administrator');
+        $this->addArgument('username', InputArgument::REQUIRED, 'Admin name.');
+        $this->addArgument('password', InputArgument::REQUIRED, 'Password');
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln([
+            'Admin Creator',
+            '=============',
+            '',
+        ]);
+
+        $admin = new Admin();
+        
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            $input->getArgument('password')
+        );
+
+        $admin->setUsername($input->getArgument('username'));
+        $admin->setPassword($hashedPassword);
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $this->manager->persist($admin);
+        $this->manager->flush();
+        
+        $output->writeln('Admin has been created');
+
+        return Command::SUCCESS;
     }
 }
